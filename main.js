f2af4c2670121fdf077a76f382b9910149c1873c
johnny1204 <major@za2.so-net.ne.jp>
2018-08-23 21:54:28 +0900

    声変更

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const http = require('http')
 const Client = require('@line/bot-sdk').Client
 const config = require('./config/setting.json')
 const ngrok = require('ngrok')
+const VoiceText = require('voicetext')
 
 googlehome.device('Google-Home', 'ja')
 googlehome.ip(config.googlehome_ip)
@@ -11,6 +12,12 @@ const client = new Client({
   channelAccessToken: config.access_token,
   channelSecret: config.client_secret
 })
+
+function notify(message){
+  googlehome.notify(message, function(text) {
+    console.log(text)
+  }
+}
 
 http.createServer(function(request, response){
   let data = ''
@@ -21,13 +28,17 @@ http.createServer(function(request, response){
   request.on("end", function() {
     const line = JSON.parse(data).events[0]
     if(line.type != 'message' || line.message.type != 'text') return
-
-    client.getProfile(line.source.userId).then(function(data){
-      const message = data.displayName + 'からメッセージです。' + line.message.text
-      googlehome.notify(message, function(text) {
-        console.log(text)
-      })
-    })
+    console.log(line.source.userId)
+    const user = line.users[line.source.userId]
+    if(user){
+      const message = user + 'からメッセージです。' + line.message.text
+      if(line.voice_api_key){
+        const voice = new VoiceText(line.voice_api_key)
+        voice.speaker(voice.SPEAKER.HIKARI).speak(notify(message))
+      } else {
+        notify(message)
+      }
+    }
   })
   response.writeHead(200, {"Content-Type": "text/plain"})
   response.end()
